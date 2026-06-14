85e50aa05e72ec5dc4f5d18e38c5ee0516f2384c
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-06-14 18:15:14 +0500

    working on guardli

---
+++ app/ai.py
@@ -0,0 +1,35 @@
+import json
+
+from google import genai
+
+from app.config import (
+    GOOGLE_API_KEY,
+    MODEL_NAME
+)
+
+from app.prompts import SYSTEM_PROMPT
+
+
+client = genai.Client(
+    api_key=GOOGLE_API_KEY
+)
+
+
+def classify_content(text: str):
+
+    prompt = f"""
+{SYSTEM_PROMPT}
+
+Content:
+
+{text}
+"""
+
+    response = client.models.generate_content(
+        model=MODEL_NAME,
+        contents=prompt
+    )
+
+    raw = response.text.strip()
+
+    return json.loads(raw)
